c66fc8d37680005642eab0fef5ec99fbe6fefe86
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-17 08:05:42 -0500

    testing

--- parse.rb
+++ parse.rb
@@ -1,12 +1,9 @@
 require "pikelet"
 require "pry"
+require "./lib/airports.rb"
 
 data_dir = "./subscription-data"
 airports_file = "#{data_dir}/APT.txt"
-
-airport_definition = Pikelet.define do
-  faa_lid 27...30
-end
 
 lines = []
 File.open(airports_file).each_line do |line|
@@ -17,5 +14,6 @@ lines_airports = lines.select do |line|
   line.start_with? "APT"
 end
 
-airports = airport_definition.parse(lines_airports)
-binding.pry
+airports = Airports.parse(lines_airports)
+
+puts airports.select { |ap| ap.icao }
