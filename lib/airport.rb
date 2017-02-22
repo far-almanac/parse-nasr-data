554688dd1eda50d7153e21fe9fe1cbd33e07a478
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-21 17:49:32 -0800

    airport timezone works

--- lib/airport.rb
+++ lib/airport.rb
@@ -17,13 +17,17 @@ class Airport
     @longitude_dd = DmsCoordinate.new_from_s(longitude).to_dd
   end
 
-  %w(identifier airport_name city country icao elevation latitude longitude)
+  %w(identifier airport_name city icao elevation latitude longitude)
   .each do |deferred_method|
     define_method(deferred_method) do
       @airport_pikelet_struct.send(deferred_method)
     end
   end
 
+  def country
+  end
+
   def timezone
+    Timezone.lookup(latitude_dd, longitude_dd)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -7,21 +7,20 @@ RSpec.describe Airport do
     all_lines = File.open(test_file)
     airport_lines = Airports.airport_lines(all_lines)
     airports = Airports.parse(airport_lines)
-    airport_pikelet_struct = airports.first
+    airport_pikelet_struct = airports[0]
     @airport = Airport.new(airport_pikelet_struct)
+    airport_pikelet_struct2 = airports[1]
+    @airport2 = Airport.new(airport_pikelet_struct2)
   end
 
   context "has a/an" do
-    it "identifier" do
-      expect(@airport.identifier).to eq("ADK")
+    it "country" do
     end
 
-    it "airport name" do
-      expect(@airport.airport_name).to eq("ADAK")
-    end
-
-    it "city" do
-      expect(@airport.city).to eq("ADAK ISLAND")
+    it "timezone" do
+      expect(@airport.timezone).to be_a(Timezone::Zone)
+      expect(@airport.timezone.to_s).to eq("America/Adak")
+      expect(@airport2.timezone.to_s).to eq("America/Chicago")
     end
   end
 end
