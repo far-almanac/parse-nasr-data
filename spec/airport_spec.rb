8f9314ef986a625e631895deda61cba946c6a180
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-21 16:16:15 -0800

    progress on airport model

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,27 @@
+require "airports"
 require "airport"
 
 RSpec.describe Airport do
   before(:all) do
-    airport_pikelet_struct = nil
+    test_file = "./spec/subscription-data/APT.txt"
+    all_lines = File.open(test_file)
+    airport_lines = Airports.airport_lines(all_lines)
+    airports = Airports.parse(airport_lines)
+    airport_pikelet_struct = airports.first
     @airport = Airport.new(airport_pikelet_struct)
   end
 
-  it "foo" do
-    expect(@airport.identifier).to eq("ADK")
+  context "has a/an" do
+    it "identifier" do
+      expect(@airport.identifier).to eq("ADK")
+    end
+
+    it "airport name" do
+      expect(@airport.airport_name).to eq("ADAK")
+    end
+
+    it "city" do
+      expect(@airport.city).to eq("ADAK ISLAND")
+    end
   end
 end
