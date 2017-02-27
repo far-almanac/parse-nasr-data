0a2d7c9600559fe5836aae191efe0257efffc686
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-26 23:03:21 -0500

    improve airport testing and add bhams testing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,16 +11,34 @@ RSpec.describe Airport do
     @airport = Airport.new(airport_pikelet_struct)
     airport_pikelet_struct2 = airports[1]
     @airport2 = Airport.new(airport_pikelet_struct2)
+    airport_pikelet_struct3 = airports[2]
+    @airport3 = Airport.new(airport_pikelet_struct3)
   end
 
   context "has a/an" do
-    it "country" do
+    context "country" do
+      it "of United States" do
+        expect(@airport.country).to eq("United States")
+        expect(@airport2.country).to eq("United States")
+      end
+
+      it "of The Bahamas" do
+        expect(@airport3.country).to eq("The Bahamas")
+      end
     end
 
-    it "timezone" do
-      expect(@airport.timezone).to be_a(Timezone::Zone)
-      expect(@airport.timezone.to_s).to eq("America/Adak")
-      expect(@airport2.timezone.to_s).to eq("America/Chicago")
+    context "timezone" do
+      it "and returns s Timezone::Zone object" do
+        expect(@airport.timezone).to be_a(Timezone::Zone)
+      end
+
+      it "and finds America/Adak timezone" do
+        expect(@airport.timezone.to_s).to eq("America/Adak")
+      end
+
+      it "and finds America/Chicago timezone" do
+        expect(@airport2.timezone.to_s).to eq("America/Chicago")
+      end
     end
   end
 end
